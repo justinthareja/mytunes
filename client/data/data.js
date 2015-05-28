dd86895434fb42776b34fd0c33ab54374388cb7d
Justin Thareja <justin.thareja@gmail.com>
2015-05-28 16:44:43 -0700

    Update data.js

--- client/data/data.js
+++ client/data/data.js
@@ -1,23 +1,55 @@
 // data.js - Defines an array of data regarding song files and their accompanying details.
 var songData = [
+  // {
+  //   url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/04+One+In+A+Million.mp3",
+  //   title: "One In A Million",
+  //   artist: "Aaliyah",
+  // },
+  // {
+  //   url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/03+Age+Ain%27t+Nothing+But+A+Number.mp3",
+  //   title: "Age Ain't Nothing But A Number",
+  //   artist: "Aaliyah",
+  // },
+  // {
+  //   url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/05+Hot+Like+Fire.mp3",
+  //   title: "Hot Like Fire",
+  //   artist: "Aaliyah",
+  // },
+  // {
+  //   url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/06+If+Your+Girl+Only+Knew.mp3",
+  //   title: "If Your Girl Only Knew",
+  //   artist: "Aaliyah",
+  // }
   {
-    url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/04+One+In+A+Million.mp3",
-    title: "One In A Million",
-    artist: "Aaliyah",
+    url: "http://walrusmusicblog.com/wp-content/uploads/2009/11/Lets-Go-Surfing.mp3",
+    title: "Let's Go Surfing",
+    artist: "The Drums"
   },
+
   {
-    url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/03+Age+Ain%27t+Nothing+But+A+Number.mp3",
-    title: "Age Ain't Nothing But A Number",
-    artist: "Aaliyah",
+    url: "http://www.viscidity.com/stuffs/stoofoomusicpost/3/04.%20Terminally%20Chill.mp3",
+    title: "Terminally Chill",
+    artist: "Neon Indian"
   },
+
   {
-    url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/05+Hot+Like+Fire.mp3",
-    title: "Hot Like Fire",
-    artist: "Aaliyah",
+    url: "http://static.stereogum.com/blogs.dir/2/files/mp3/Washed%20Out%20-%20Feel%20It%20All%20Around.mp3",
+    title: "Feel It All Around",
+    artist: "Washed Out"
   },
+
   {
-    url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/06+If+Your+Girl+Only+Knew.mp3",
-    title: "If Your Girl Only Knew",
-    artist: "Aaliyah",
-  }
+    url: "http://119.145.149.47/fs.vip.pc.kugou.com/201505290729/d2f1def38d0a02f4753fcc2d85d075ac/G011/M0A/1F/08/Sw0DAFTVF5GAcPlNAFHLRcJ_X9U231.mp3",
+    title: "No Waves",
+    artist: "FIDLAR"
+  },
+
+  {
+    url: "http://www.passionweiss.com/wp-content/uploads/2008/04/09-dirt-featuring-aesop-rock-1.mp3",
+    title: "Dirt",
+    artist: "Tobacco"
+  },
+
+  
+
 ];
